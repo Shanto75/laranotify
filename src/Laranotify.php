b16d46dcdcd07d76458ef11fbd2ef4e07093484c
Moniruzzaman Shanto <42086179+Shanto75@users.noreply.github.com>
2024-03-13 21:52:21 +0600

    updated

--- src/Laranotify.php
+++ src/Laranotify.php
@@ -4,9 +4,46 @@ namespace Shanto75\Laranotify;
 
 class Laranotify
 {
-    public function notify($message)
+    protected static $message;
+    protected static $type;
+    protected static $color;
+
+    public static function notify($message)
     {
-        session()->flash('message', $message);
+        self::$message = $message;
+        return new self();
     }
-    
+
+    public static function warning()
+    {
+        self::$type = 'warning';
+        self::$color = '#ffc107';
+        return new self();
+    }
+
+    public static function success()
+    {
+        self::$type = 'successful';
+        self::$color = '#198754';
+        return new self();
+    }
+
+    public static function error()
+    {
+        self::$type = 'failed';
+        self::$color = '#dc3545';
+        return new self();
+    }
+
+    public static function getMessage(){
+        return self::$message;
+    }
+
+    public static function getType(){
+        return self::$type;
+    }
+
+    public static function getColor(){
+        return self::$color;
+    }
 }
